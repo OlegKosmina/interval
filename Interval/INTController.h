22657f0df9e865868cc1b25a0115d27f803b8e23
Ichi Kanaya <kanaya@pineappledesign.org>
2012-09-22 10:45:18 +0900

    Plain view.

---
+++ Interval/INTController.h
@@ -0,0 +1,13 @@
+//
+//  INTController.h
+//  Interval
+//
+//  Created by Ichi Kanaya on 22/09/2012.
+//  Copyright (c) 2012 Ichi Kanaya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface INTController : NSObject
+
+@end
---
+++ Interval/INTController.m
@@ -0,0 +1,13 @@
+//
+//  INTController.m
+//  Interval
+//
+//  Created by Ichi Kanaya on 22/09/2012.
+//  Copyright (c) 2012 Ichi Kanaya. All rights reserved.
+//
+
+#import "INTController.h"
+
+@implementation INTController
+
+@end
